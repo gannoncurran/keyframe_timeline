e968885ebd83b6b1dae5873d6af62033f36ed5d4
Gannon Curran <gannoncurran@gmail.com>
2015-04-14 18:36:16 -0700

    react skeleton

---
+++ src/ComponentName.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var ComponentName = React.createClass({
+
+	render: function() {
+		return (
+			<div>React Skeleton 2014.04.14</div>
+		);
+	}
+
+});
+
+module.exports = ComponentName;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+var React = require('react');
+var ComponentName = require('./ComponentName.js');
+
+React.render(<ComponentName />, document.body);
